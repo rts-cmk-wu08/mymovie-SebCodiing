6b277eb21dda4b0793836ea9d48c2b6400f0edb9
Sebastianohansen <U2420664@rts-365.dk>
2022-09-19 10:36:37 +0200

    jeg har tilføjet en ekstra fetch som jeg har kaldt upcoming

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const API_KEY = ''
+const API_KEY = '3a4fcefe46f3db1f6864e930d246d190'
 const BASE_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=a068dcb586f22a44b0c64b1b1be088eb&language=en-US&page=1'
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' 
+const upcoming = BASE_URL + '/discover/movie?sort_by=upcoming.desc&'
 
-getMovies(API_URL);
+let wrapperElm = document.querySelector(".wrapper")
 
-function getMovies(url) {
+let showingElm = document.createElement("section")
+showingElm.classList.add("showing")
+wrapperElm.append(showingElm)
 
-        fetch(url).then(res => res.json()).then(data => {
+let upcomingElm = document.createElement("section")
+upcomingElm.classList.add("upcoming")
+wrapperElm.append(upcomingElm)
+
+
+
+
+fetch(API_URL).then(res => res.json()).then(data => {
             console.log(data);
             showMovies(data.results);
 
 
+})
 
-        })
-}
+fetch(upcoming).then(res => res.json()).then(data => {
+    console.log(data);
+    upcomingMovies(data.results);
 
+
+})
 
 function showMovies(data) {
 
@@ -26,6 +40,21 @@ function showMovies(data) {
         <h1>${movie.title}</h1>
         `
 
-        document.body.append(movieElm)
+        showingElm.append(movieElm)
     })
+}
+
+function upcomingMovies(data) {
+
+    data.forEach(movie => {
+        const movieElm = document.createElement('div');
+        movieElm.classList.add('upcoming');
+        movieElm.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+        <h1>${movie.title}</h1>
+        `
+
+        upcomingElm.append(movieElm)
+    })
+
 }
